6c0b74afca332e393851acea1489922bf6299d04
Vince Velocci <velocci@utexas.edu>
2016-08-14 01:04:02 -0500

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,20 @@
-PLACE YOUR CODE HERE
+import string
+import csv
+
+
+file = open('faculty.csv')
+reader = csv.reader(file)
+data = list(reader)
+data[0]=[word.strip(' ') for word in data[0]]
+
+email_index = data[0].index('email')
+
+emails = []
+
+for i in range(1,len(data)):
+	emails.append(data[i][email_index])
+
+output_file = open('emails.csv', 'w')
+wr = csv.writer(output_file)
+for item in emails:
+	wr.writerow([item])
