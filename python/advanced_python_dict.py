9f2bc6dd56fca66cfe277b24d2757abe014cd29b
Vince Velocci <velocci@utexas.edu>
2016-08-14 18:13:21 -0500

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,5 @@
-PLACE YOUR CODE HERE
+faculty_dict = { 'Ellenberg': [['Ph.D.', 'Professor', 'sellenbe@upenn.edu'], ['Ph.D.', 'Professor', 'jellenbe@mail.med.upenn.edu']],
+              'Li': [['Ph.D.', 'Assistant Professor', 'liy3@email.chop.edu'], ['Ph.D.', 'Associate Professor', 'mingyao@mail.med.upenn.edu'], ['Ph.D.', 'Professor', 'hongzhe@upenn.edu']]}
+
+first3vals = [(k,faculty_dict[k]) for k in sorted(faculty_dict.keys())[:3]]
+print(first3vals)
