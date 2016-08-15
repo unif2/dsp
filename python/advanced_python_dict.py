4d1aebeba9df7098300e4fa27009b8666e331a59
Vince Velocci <velocci@utexas.edu>
2016-08-14 23:12:45 -0500

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -8,3 +8,12 @@ professor_dict = {('Susan', 'Ellenberg'): ['Ph.D.', 'Professor', 'sellenbe@upenn
 
 first3 = [(k,professor_dict[k]) for k in sorted(professor_dict.keys())[:3]]
 print(first3)
+
+professor_dict = {('Susan', 'Ellenberg'): ['Ph.D.', 'Professor', 'sellenbe@upenn.edu'], ('Jonas', 'Ellenberg'): ['Ph.D.', 'Professor', 'jellenbe@mail.med.upenn.edu'], ('Yimei', 'Li'): ['Ph.D.', 'Assistant Professor', 'liy3@email.chop.edu'], ('Mingyao','Li'): ['Ph.D.', 'Associate Professor', 'mingyao@mail.med.upenn.edu'], ('Hongzhe','Li'): ['Ph.D.', 'Professor', 'hongzhe@upenn.edu'] }
+d = {}
+for key,value in professor_dict.items():
+	key = tuple(reversed(key))
+	d[key] = value
+
+for key, value in sorted(d.items()):
+	print(key,value)
